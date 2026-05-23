580936686acb8ee652c299d8c9f26de41b83b6cc
bilel.bennour <bilel.bennour23@gmail.com>
2026-05-23 05:21:35 +0100

    remove

--- middleware.ts
+++
@@ -1,3 +0,0 @@
-// This file has been migrated to proxy.ts (Next.js 16+ convention)
-// See: https://nextjs.org/docs/app/building-your-application/routing/middleware
-// The proxy.ts file contains the middleware configuration
